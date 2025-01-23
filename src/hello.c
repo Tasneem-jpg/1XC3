67dca35ace79f39d3d67d95a1b5c053ad12f3f9c
Tasneem-jpg <kanchwat@mcmaster.ca>
2025-01-23 17:26:14 -0500

    Added user input for name

--- src/hello.c
+++ src/hello.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
-int main()
+#include <stdio .h>
+2 int main()
 {
-    printf("Hello, World!\n ");
-    return 0;
+    3 char name[50];
+    4 printf(" Please enter your name : ");
+    5 scanf(" % s ", name);
+    6 printf(" Hello , % s !\n ", name);
+    7 return 0;
+    8
 }
